74de54271504ae7dcb2e17ae37665492b51d5ffd
User020606 <vincentmoua06@gmail.com>
2025-08-28 10:51:58 -0700

    Add files via upload

---
+++ MaddnessWithMethods.java
@@ -0,0 +1,26 @@
+
+        // MaddnessWithMethods.java
+        // vM 8/28/25
+        // Refernces: w3schools: how to add two numbers, and user input
+
+import java.util.Scanner;
+
+    public class MaddnessWithMethods {
+        public static void main(String[] args) {
+
+            int num1 = 0;
+            int num2 = 0;
+            Scanner scanner = new Scanner(System.in);
+
+
+            System.out.print("Enter a number: ");
+            num1 = scanner.nextInt();
+
+            System.out.print("Enter a second number: ");
+            num2 = scanner.nextInt();
+            int sum = num1 + num2;
+            int rest = num1 - num2;
+            System.out.println( " num1 + num2 is " + sum);
+            System.out.println( " num1 - num2 is " + rest);
+        }
+}
