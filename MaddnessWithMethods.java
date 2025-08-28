9bde00442ff1f610055954db29a7c97b3c9e275d
User020606 <vincentmoua06@gmail.com>
2025-08-28 10:46:48 -0700

    Add files via upload

---
+++ MaddnessWithMethods.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+    public class MaddnessWithMethods {
+        public static void main(String[] args) {
+
+            int num1 = 0;
+            int num2 = 0;
+            Scanner scanner = new Scanner(System.in);
+
+
+            System.out.print("Enter a number: ");
+            num1 = scanner.nextInt();
+
+            System.out.print("Enter a second number: ");
+            num2 = scanner.nextInt();
+            int sum = num1 + num2;
+            int rest = num1 - num2;
+            System.out.println( " num1 + num2 is " + sum);
+            System.out.println( " num1 - num2 is " + rest);
+        }
+}
